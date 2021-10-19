bc13ee8bfff0090634825d3e1c7db50e27e16f83
strawberry-rhubarb <amelia.and.speck@gmail.com>
2021-10-18 17:29:29 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Amelia McKee ,agmckee@ucsc.edu>
+ * Created:   10.18.2021
+ * Lisence public domain
+ */
+
+//Define Varriables
+make = "cheverolet";
+model = "parkwood";
+color = "blue";
+year = "1959";
+
+//Calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");

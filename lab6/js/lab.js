e8c042151437e6b8d483d875e9a281425fcb535e
strawberry-rhubarb <amelia.and.speck@gmail.com>
2021-10-20 10:00:32 -0700

    lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,19 @@
+/*
+ * Author:    Amelia McKee ,agmckee@ucsc.edu>
+ * Created:   10.18.2021
+ * Lisence public domain
+ */
+
+ //Define Varriables
+ var myTransport = ["walking", "biking", "bussing", "carpool"];
+ var myMainRide = {};
+  myMainRide.name = "ebike";
+  myMainRide.color = "white";
+  myMainRide.size = "medium";
+  myMainRide.model = "pace 500 step through";
+  myMainRide.make = "aventon";
+
+//output
+document.writeln("Getting around: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");

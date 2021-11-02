8352bb979da65c51bcca5fdcbf001ce74cee5699
strawberry-rhubarb <amelia.and.speck@gmail.com>
2021-11-02 08:42:53 -0700

    changes in class

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,9 +3,17 @@
  * Created:   11.01.2021
  * Lisence public domain
  */
-var bioEl = document.getElementById("output");
-var targetEl=document.getElementById("output");
-var new1El = document.createElement("button")
+
+//  Use getElementById() to find your output <div> and assign it to a variable outputEl
+// Create a new element with document.createElement("p") and assign it to a variable new1El
+// Change the html attribute of new1El to say something new.
+// Create another new element and assign it to a variable new2El
+// Change the html attribute of new2El to say something else.
+// Append both new elements one at a time using appendChild() to your output <div>
+// Change the css attributes of at least two elements of your page
+
+var outputEl = document.getElementById("output");
+var new1El = document.createElement("button");
 new1El.id = "button";
 new1El.innerHTML = "Press me!";
 document.body.appendChild(new1El);

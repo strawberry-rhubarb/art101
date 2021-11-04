9dd06e1438fffebfa6d8cc5572ee89002af26d54
strawberry-rhubarb <amelia.and.speck@gmail.com>
2021-11-03 22:37:54 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,24 @@
+
+/*
+ * Author:    Amelia McKee ,agmckee@ucsc.edu>
+ * Created:   11.01.2021
+ * Lisence public domain
+ */
+
+function putNameInTitle() {
+  // When the button is pushed,
+  // prompt the user to input their name
+  // with the prompt() function.
+  //var name = prompt("Please enter your name:");
+  var field = document.getElementById("your name");
+  var name = field.value;
+
+  // Use this input to change the text from
+  // ‘Hello, World!’ to ‘Hello’ + the user’s name
+  var headingEl = document.getElementById("title");
+  headingEl.innerHTML = "Hello, " + name;
+}
+
+// Create an event handler within the JS file that listens for a button click.
+var buttonEl = document.getElementById("mybutton");
+buttonEl.addEventListener("click", putNameInTitle);
